abf1a9c9edf4c4ee99c1b09411bfaf037a027e4a
elliottcable <git@elliottcable.name>
2009-06-16 16:56:58 -0800

    Completely re-worked the Rakefile, using a lot from Nfoiled's latest Rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,116 +1,132 @@
+LibName = "stringray"
 ($:.unshift File.expand_path(File.join( File.dirname(__FILE__), 'lib' ))).uniq!
-require 'stringray'
-require 'extlib/string' # String#/, because I'm a lazy fuck.
-require 'rake'
-require 'yard'
-require 'yard/rake/yardoc_task'
-require 'spec/rake/spectask'
-require 'spec/rake/verify_rcov'
-require 'stringray/core_ext/spec/rake/verify_rcov'
+require LibName
+Lib = StringRay
 
+default_tasks = Array.new
+
+# =======================
+# = Gem packaging tasks =
+# =======================
 begin
   require 'echoe'
   
-  task :package => :'package:package'
+  default_tasks << :'package:manifest' unless ENV['CI']
+  default_tasks << :'package:verify'
+  
   task :install => :'package:install'
+  task :package => :'package:package'
   task :manifest => :'package:manifest'
   namespace :package do
-    Echoe.new('stringray', StringRay::Version) do |g|
-      g.project = 'stringray'
+    Echoe.new(LibName, Lib::Version) do |g|
       g.author = ['elliottcable']
-      g.email = ['StringRay@elliottcable.com']
+      g.email = ["#{LibName}@elliottcable.com"]
       g.summary = 'Combining many of the benefits of Arrays and Strings, StringRay allows you to treat a String as an Array of words in many cases.'
-      g.url = 'http://github.com/elliottcable/stringray'
-      g.dependencies = []
-      g.development_dependencies = ['elliottcable-echoe >= 3.0.2', 'rspec', 'rcov', 'yard']
+      g.url = 'http://github.com/elliottcable/' + LibName
+      g.runtime_dependencies = []
+      g.development_dependencies = ['echoe >= 3.0.2', 'rspec', 'rcov', 'yard']
       g.manifest_name = '.manifest'
       g.retain_gemspec = true
       g.rakefile_name = 'Rakefile.rb'
-      g.ignore_pattern = /(^\.git$|^\.yardoc$|^meta\/|\.gemspec$)/
+      g.ignore_pattern = /^\.git\/|^meta\/|\.gemspec/
     end
   
-    desc 'tests packaged files to ensure they are all present'
+    desc 'Tests packaged files to ensure they are all present'
     task :verify => :package do
       # An error message will be displayed if files are missing
-      if system %(ruby -e "require 'rubygems'; require 'pkg/stringray-#{StringRay::VERSION}/lib/stringray'")
-        puts "\nThe library files are present"
+      if system %(ruby -e "require 'rubygems'; require 'pkg/#{LibName}-#{Lib::Version}/lib/#{LibName}'")
+        puts "\nThe library files are present."
       end
     end
   end
   
-rescue LoadError => boom
-  puts "You are missing a dependency required for meta-operations on this gem."
-  puts "#{boom.to_s.capitalize}."
-ensure
-  task :default # No effect # Invisible
+rescue LoadError => exception
+  raise exception unless exception.message =~ /(echoe)$/
+  desc '!! You need the `echoe` gem to package or install this project!'
+  task :package
+end
 
-  # Runs specs, generates rcov, and opens rcov in your browser.
-  namespace :rcov do
+# =======================
+# = Spec/Coverage tasks =
+# =======================
+begin
+  require 'spec'
+  require 'rcov'
+  require 'spec/rake/spectask'
+  
+  default_tasks << :'coverage:run'
+  default_tasks << :'coverage:open' unless ENV['CI']
+  
+  task :coverage => :'coverage:run'
+  namespace :coverage do
     Spec::Rake::SpecTask.new(:run) do |t|
-      t.spec_opts = ["--format", "specdoc", "--colour"]
-      t.spec_files = Dir['spec/**/*_spec.rb'].sort
-      t.libs = ['lib']
-      t.rcov = true
-      t.rcov_opts = ['--exclude-only', '".*"', '--include-file', '^lib']
-      t.rcov_dir = 'meta' / 'coverage'
-    end
-
-    Spec::Rake::SpecTask.new(:plain) do |t|
       t.spec_opts = ["--format", "specdoc"]
-      t.spec_files = Dir['spec/**/*_spec.rb'].sort
+      t.spec_opts << "--colour" unless ENV['CI']
+      t.spec_files = Dir[File.join('spec', '**', '*_spec.rb')].sort
       t.libs = ['lib']
       t.rcov = true
-      t.rcov_opts = ['--exclude-only', '".*"', '--include-file', '^lib']
-      t.rcov_dir = 'meta' / 'coverage'
+      t.rcov_opts = ['--exclude-only', '".*"', '--include-file', '"^lib"',
+        '--sort', 'loc', '--sort-reverse',
+        '--comments', '--profile', '--text-report', '-w' ]
+      t.rcov_dir = File.join('meta', 'coverage')
     end
-
-    RCov::VerifyTask.new(:verify) do |t|
-      t.threshold = 98.7
-      t.index_html = 'meta' / 'coverage' / 'index.html'
-      t.require_exact_threshold = false
-    end
-
+    
     task :open do
-      system 'open ' + 'meta' / 'coverage' / 'index.html' if PLATFORM['darwin']
-    end
+      system 'open ' + File.join('meta', 'coverage', 'index.html')
+    end if RUBY_PLATFORM =~ /darwin/
   end
   
-  namespace :yard do
+rescue LoadError => exception
+  raise exception unless exception.message =~ /(rcov|spec)$/
+  desc '!! You need the `rcov` and `rspec` gems to run specs or coverage!'
+  task :coverage
+end
+
+# =======================
+# = Documentation tasks =
+# =======================
+begin
+  require 'yard'
+  require 'yard/rake/yardoc_task'
+  
+  default_tasks << :'documentation:generate'
+  default_tasks << :'documentation:open' unless ENV['CI']
+  
+  task :documentation => :'documentation:generate'
+  namespace :documentation do
     YARD::Rake::YardocTask.new :generate do |t|
-      t.files   = ['lib/**/*.rb']
-      t.options = ['--output-dir', "meta/documentation", '--readme', 'README.markdown']
+      t.files   = [File.join('lib', '**', '*.rb')]
+      t.options = ['--output-dir', File.join('meta', 'documentation'),
+                   '--readme', 'README.markdown']
     end
     
-    YARD::Rake::YardocTask.new :dot_yardoc do |t|
-      t.files   = ['lib/**/*.rb']
-      t.options = ['--no-output', '--readme', 'README.markdown']
+    YARD::Rake::YardocTask.new :yardoc do |t|
+      t.files   = [File.join('lib', '**', '*.rb')]
+      t.options = ['--no-output',
+                   '--readme', 'README.markdown']
     end
     
     task :open do
-      system 'open ' + 'meta' / 'documentation' / 'index.html' if PLATFORM['darwin']
-    end
+      system 'open ' + File.join('meta', 'documentation', 'index.html')
+    end if RUBY_PLATFORM =~ /darwin/
   end
   
-  namespace :git do
-    task :status do
-      `git status`
-    end
-    
-    task :commit => [:'echoe:manifest', :'yard:dot_yardoc'] do
-      `git commit`
-    end
-  end
-  
-  task :clobber => [:'echoe:clobber_package', :'echoe:clobber_docs', :'echoe:clobber_coverage'] do
-    rm_f 'meta'
-  end
+rescue LoadError => exception
+  raise exception unless exception.message =~ /(yard)$/
+  desc '!! You need the `yard` gem to generate documentation!'
+  task :documentation
+end
 
-  desc 'Check everything over before commiting'
-  task :aok => [:'yard:generate', :'yard:open',
-                :'package:manifest', :'package:package',
-                :'rcov:run', :'rcov:verify', :'rcov:open',
-                :'git:status']
+# =================
+# = Miscellaneous =
+# =================
+desc 'Removes all meta producs'
+task :clobber do
+  `rm -rf #{File.expand_path(File.join( File.dirname(__FILE__), 'meta' ))} #{File.expand_path(File.join( File.dirname(__FILE__), 'pkg' ))}`
+end
 
-  # desc 'Task run during continuous integration' # Invisible
-  task :ci => [:'yard:generate', :'rcov:plain', :'rcov:verify']
+desc 'Run all default tasks'
+task :default => default_tasks do
+  puts "** Successfully ran the following tasks:"
+  puts "** #{Rake.application.tasks.select {|t| t.name == 'default' }.first.prerequisites.join(', ')}"
 end
--- stringray.gemspec
+++ stringray.gemspec
@@ -6,11 +6,11 @@ Gem::Specification.new do |s|
 
   s.required_rubygems_version = Gem::Requirement.new(">= 1.2") if s.respond_to? :required_rubygems_version=
   s.authors = ["elliottcable"]
-  s.date = %q{2009-06-11}
+  s.date = %q{2009-06-16}
   s.description = %q{Combining many of the benefits of Arrays and Strings, StringRay allows you to treat a String as an Array of words in many cases.}
-  s.email = ["StringRay@elliottcable.com"]
-  s.extra_rdoc_files = ["lib/stringray.rb", "lib/stringray/includes.rb", "lib/stringray/core_ext.rb", "lib/stringray/core_ext/kernel.rb", "lib/stringray/core_ext/spec/rake/verify_rcov.rb", "README.markdown"]
-  s.files = ["lib/stringray.rb", "lib/stringray/includes.rb", "lib/stringray/core_ext.rb", "lib/stringray/core_ext/kernel.rb", "lib/stringray/core_ext/spec/rake/verify_rcov.rb", "spec/stringray_spec.rb", "README.markdown", "Rakefile.rb", ".manifest", "stringray.gemspec"]
+  s.email = ["stringray@elliottcable.com"]
+  s.extra_rdoc_files = ["lib/stringray/core_ext/kernel.rb", "lib/stringray/core_ext.rb", "lib/stringray/includes.rb", "lib/stringray.rb", "README.markdown"]
+  s.files = ["lib/stringray/core_ext/kernel.rb", "lib/stringray/core_ext.rb", "lib/stringray/includes.rb", "lib/stringray.rb", "Rakefile.rb", "README.markdown", "spec/stringray_spec.rb", ".manifest", "stringray.gemspec"]
   s.has_rdoc = true
   s.homepage = %q{http://github.com/elliottcable/stringray}
   s.rdoc_options = ["--line-numbers", "--inline-source", "--title", "Stringray", "--main", "README.markdown"]
@@ -24,18 +24,18 @@ Gem::Specification.new do |s|
     s.specification_version = 2
 
     if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.2.0') then
-      s.add_development_dependency(%q<elliottcable-echoe>, [">= 0", "= 3.0.2"])
+      s.add_development_dependency(%q<echoe>, [">= 0", "= 3.0.2"])
       s.add_development_dependency(%q<rspec>, [">= 0"])
       s.add_development_dependency(%q<rcov>, [">= 0"])
       s.add_development_dependency(%q<yard>, [">= 0"])
     else
-      s.add_dependency(%q<elliottcable-echoe>, [">= 0", "= 3.0.2"])
+      s.add_dependency(%q<echoe>, [">= 0", "= 3.0.2"])
       s.add_dependency(%q<rspec>, [">= 0"])
       s.add_dependency(%q<rcov>, [">= 0"])
       s.add_dependency(%q<yard>, [">= 0"])
     end
   else
-    s.add_dependency(%q<elliottcable-echoe>, [">= 0", "= 3.0.2"])
+    s.add_dependency(%q<echoe>, [">= 0", "= 3.0.2"])
     s.add_dependency(%q<rspec>, [">= 0"])
     s.add_dependency(%q<rcov>, [">= 0"])
     s.add_dependency(%q<yard>, [">= 0"])
